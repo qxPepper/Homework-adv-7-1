36b9f3c402f9a4780cd3b4b49c4f1ece431d0699
Vitaly Gromov <vitaly.gro@gmail.com>
2021-10-20 21:42:44 +0300

    Коммит тестов с реализацией Homework-adv-7-1

--- src/main/java/ru/netology/CreditCalculator.java
+++ src/main/java/ru/netology/CreditCalculator.java
@@ -1,33 +1,43 @@
 package ru.netology;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class CreditCalculator {
     public double rounding(double input, int count) {
         //округление
-        return 0.0;
+        BigDecimal result = new BigDecimal(input);
+        result = result.setScale(count, RoundingMode.HALF_UP);
+
+        return result.doubleValue();
     }
 
     public double monthlyPercentage(double rateInYear) {
         // i=y/12/100
-        return 0.0;
+        final int MONTHS_IN_YEAR = 12;
+        final int ALL_PERCENTS = 100;
+
+        return rateInYear / MONTHS_IN_YEAR / ALL_PERCENTS;
     }
 
     public double annuityRatio(double monthlyRate, int allMonths) {
         // k=i*(1+i)^n/((1+i)^n-1)
-        return 0.0;
+        return monthlyRate * Math.pow(1 + monthlyRate, allMonths) /
+                (Math.pow(1 + monthlyRate, allMonths) - 1);
     }
 
     public double monthlyPayment(double annuityRatio, double creditSum) {
         // m=k*s
-        return 0.0;
+        return annuityRatio * creditSum;
     }
 
     public double totalAmount(double monthlyPayment, int allMonths) {
         // t=m*n
-        return 0.0;
+        return monthlyPayment * allMonths;
     }
 
     public double overpayment(double totalAmount, double creditSum) {
         // o=t-s
-        return 0.0;
+        return totalAmount - creditSum;
     }
 }
--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -5,8 +5,29 @@ public class Main {
         double creditSum = 1_000_000;
         int allMonths = 36;
         double rateInYear = 20;
+        int count = 5;
 
         System.out.println("Сумма кредита - " + creditSum + " руб., срок - " +
                 allMonths + " месяцев, ставка в год - " + rateInYear + " %.");
+
+        CreditCalculator calculator = new CreditCalculator();
+
+        // Рассчет месячного платежа.
+        double monthlyRate = calculator.monthlyPercentage(rateInYear);
+        monthlyRate = calculator.rounding(monthlyRate, count);
+
+        double annuityRatio = calculator.annuityRatio(monthlyRate, allMonths);
+        annuityRatio = calculator.rounding(annuityRatio, count);
+
+        double monthlyPayment = calculator.monthlyPayment(annuityRatio, creditSum);
+        System.out.println("Ежемесячные выплаты - " + monthlyPayment + " руб.");
+
+        // Рассчет общей суммы к возврату в банк.
+        double totalAmount = calculator.totalAmount(monthlyPayment, allMonths);
+        System.out.println("Общая сумма к возврату в банк - " + totalAmount + " руб.");
+
+        // Рассчет переплаты за весь период.
+        double totalOverpayment = calculator.overpayment(totalAmount, creditSum);
+        System.out.println("Суммарная переплата за весь период - " + totalOverpayment + " руб.");
     }
 }
